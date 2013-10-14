699574ad345b45fa0e2885d7e8bab0b2658fc391
JunHo, Choi <yesjun@gmail.com>
2013-10-14 11:41:28 +0900

    Fixed depreciated sizeWithFont() issue in iOS 7.

--- JTGestureBasedTableView/JTTransformableTableViewCell.m
+++ JTGestureBasedTableView/JTTransformableTableViewCell.m
@@ -181,10 +181,19 @@
     self.transformableView.frame = CGRectMake(0.0, contentViewSize.height - self.finishedHeight,
                                               contentViewSize.width, self.finishedHeight);
     
-    // 05/07/2012 : Added by SungDong Kim 
-    CGSize requiredLabelSize = [self.textLabel.text sizeWithFont:self.textLabel.font
-                                               constrainedToSize:contentViewSize
-                                                   lineBreakMode:UILineBreakModeClip];
+    // Since sizeWithFont() method has been depreciated, boundingRectWithSize() method has been used for iOS 7.
+    /* if(floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1)
+     CGSize requiredLabelSize = [self.textLabel.text sizeWithFont:self.textLabel.font
+     constrainedToSize:contentViewSize
+     lineBreakMode:NSLineBreakByClipping];
+     */
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+    paragraphStyle.lineBreakMode = NSLineBreakByClipping;
+    CGRect requiredLabelRect = [self.textLabel.text boundingRectWithSize:contentViewSize options:NSStringDrawingUsesLineFragmentOrigin
+                                                              attributes:@{NSFontAttributeName:self.textLabel.font,
+                                                                           NSParagraphStyleAttributeName: paragraphStyle}
+                                                                 context:nil];
+    CGSize requiredLabelSize = requiredLabelRect.size;
     self.imageView.frame = CGRectMake(10.0 + requiredLabelSize.width + 10.0,
                                       (self.finishedHeight - self.imageView.frame.size.height)/2,
                                       self.imageView.frame.size.width,
